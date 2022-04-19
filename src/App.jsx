79d22b076a1040fc15d44ff69b1d89eaebb2c061
halitbatur <halitfuatbatur@gmail.com>
2022-04-19 03:26:36 +0300

    edit readme

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
       <div
         className="App"
         style={{ background: "#ececec", minHeight: "100vh" }}
